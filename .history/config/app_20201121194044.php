0d82bb127674bdf820b73d9010b66501f81d4b72
dilan wijerathne <dilan505@gmail.com>
2020-11-24 11:55:32 +0530

    logs

--- .history/config/app_20201121194044.php
+++ .history/config/app_20201121194044.php
@@ -161,7 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Arcanedev\LogViewer\LogViewerServiceProvider::class,
 
         /*
          * Package Service Providers...
