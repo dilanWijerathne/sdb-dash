76af2f4ac509a153516f7a5ddaefab5b8d53a3ed
dilan wijerathne <dilan505@gmail.com>
2020-12-09 14:01:23 +0530

    we

--- .history/resources/views/team.blade_20201209140100.php
+++ .history/resources/views/team.blade_20201209140100.php
@@ -114,10 +114,9 @@ console.log(branch);
       $('#example1 tbody').on( 'click', 'button', function () {
           var data = table.row( $(this).parents('tr') ).data();
           alert( data[1] +"'NIC "+ data[ 4 ] );
-          team_member_details(data[1]);
           $('#exampleModalCenter').modal('show');
         //  window.open('/sdb-dash/applicant-details?ReportID='+ data[ 4 ], '_blank');
-
+        team_member_details(data[1]);
       } );
 
 
