d9bb63e321fc26cf382ffbb0fc836c00c25c91e2
dilan wijerathne <dilan505@gmail.com>
2020-12-09 19:32:46 +0530

    deletion

---
+++ .history/routes/api_20201209193142.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::post('/applicant-approval', [DashController::class, 'approve']);
+
+
+Route::post('/new_member', [DashController::class, 'new_member']);
+
+Route::post('/review', [DashController::class, 'review']);
+
+Route::post('/comment', [DashController::class, 'comment']);
+
+Route::get('/comment_by_bdo_app', [DashController::class, 'comment_by_bdo_app']);
+
+Route::get('/nessage_by_ref', [DashController::class, 'nessage_by_ref']);
+
+Route::get('/nessage_send', [DashController::class, 'nessage_send']);
+
+Route::get('/my_team_member', [DashController::class, 'my_team_member']);
+
+Route::post('/update_my_team_member', [DashController::class, 'update_my_team_member']);
+
+Route::post('/delete_my_team_member', [DashController::class, 'delete_my_team_member']);
