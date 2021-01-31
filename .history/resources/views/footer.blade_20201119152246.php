a13bf7ec5b6d73d67de3638c7f582c544da7b8e8
dilan wijerathne <dilan505@gmail.com>
2021-01-31 17:20:00 +0530

    we

--- .history/resources/views/footer.blade_20201119152246.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
-    </div>
-    <strong>Copyright &copy; 2020 <a href="https://www.sdb.lk">SDB Bank PLC</a>.</strong> All rights
-    reserved.
-  </footer>
